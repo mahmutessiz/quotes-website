4c37b623f5831279c9a8c33eea1d97f5af5f38d7
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-02-11 12:38:06 +0300

    styling

--- quotes-website/src/components/HeaderSection.vue
+++ quotes-website/src/components/HeaderSection.vue
@@ -5,11 +5,23 @@
     class="w-full bg-lime-300 flex justify-between items-center px-4 py-2 shadow-md"
   >
     <div>
-      <img src="https://picsum.photos/100/50" alt="logo" />
+      <img
+        src="https://picsum.photos/100/50"
+        alt="logo"
+        class="cursor-pointer"
+      />
     </div>
     <div class="flex gap-2">
-      <input type="text" class="bg-white w-full" placeholder="   Search" />
-      <img src="https://picsum.photos/30/30" alt="search" />
+      <input
+        type="text"
+        class="bg-white w-full rounded-sm shadow-sm"
+        placeholder="   Search"
+      />
+      <img
+        src="https://picsum.photos/30/30"
+        class="cursor-pointer rounded-sm shadow-sm"
+        alt="search"
+      />
     </div>
     <nav>
       <ul class="flex gap-4 mr-4">
