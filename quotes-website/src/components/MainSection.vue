3d7a184a49a78b7b1401bf92a71c07db01ef9a58
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-02-20 14:58:51 +0300

    add alert after coppy

--- quotes-website/src/components/MainSection.vue
+++ quotes-website/src/components/MainSection.vue
@@ -41,6 +41,14 @@ export default {
       navigator.clipboard.writeText(event.target.innerHTML);
       // Alert the copied text
       console.log("Copied the text: " + event.target.innerHTML);
+
+      const alertContainer = document.querySelector("#alert-copy");
+      alertContainer.classList.add("grid");
+      alertContainer.classList.remove("hidden");
+      setTimeout(() => {
+        alertContainer.classList.add("hidden");
+        alertContainer.classList.remove("grid");
+      }, 2000);
     },
   },
   computed: {
@@ -174,6 +182,14 @@ export default {
           alt="background image far east"
         />
       </div>
+    </div>
+    <div
+      id="alert-copy"
+      class="fixed bottom-8 right-9 hidden w-1/2 place-items-center rounded-md bg-blue-900 py-4 px-2 transition-all duration-500"
+    >
+      <p class="text-center font-semibold text-white">
+        The quote copied to clipboard
+      </p>
     </div>
   </main>
 </template>
